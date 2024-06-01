3eaedee50245e1bafd5ab7c93b9be6713427d6f5
joshtrinh1102 <nltrinh1102@gmail.com>
2024-05-31 22:21:15 -0400

    Create Manager subclass with bonus calculation.

--- Salary calculator.js
+++ Salary calculator.js
@@ -7,3 +7,15 @@ class Employee{
     yearpay(){return this.salary *12}
     //Implement Employee class with name, salary properties, and annual salary calculation
 }
+ class Manager extends Employee{
+    constructor(name,salary,department){
+        super(name,salary);
+        this.department=department;
+        
+    }
+    yearpay(){
+        const basesalary = super.yearpay()
+        const bonus=0.15
+        console.log(`The bonus is 15% ${bonus*basesalary}`)
+        return (bonus *basesalary +basesalary)//Create Manager subclass with bonus calculation.
+ }};
