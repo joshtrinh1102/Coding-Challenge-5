d789f56648bdd7e51cdc1b99deef1d4d16260ef2
joshtrinh1102 <nltrinh1102@gmail.com>
2024-05-31 22:04:31 -0400

    Implement Employee class with name, salary properties, and annual salary calculation

---
+++ Salary calculator.js
@@ -0,0 +1,9 @@
+//U#: U22275330
+class Employee{
+    constructor(name,salary){
+        this.name=name;
+        this.salary=salary;
+        console.log(`Name: ${name} . Salary: ${salary}`)}
+    yearpay(){return this.salary *12}
+    //Implement Employee class with name, salary properties, and annual salary calculation
+}
